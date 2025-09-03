51179cf553334de78c3f0ea634c261f5f12c9e2f
Omar Iskandarani <info@omariskandarani.com>
2025-09-03 13:14:11 +0200

    Update project configuration and add initial README and LaTeX example

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# LiveLatex
+
+![Build](https://github.com/bg-omar/LiveLatex/workflows/Build/badge.svg)
+[![Version](https://img.shields.io/jetbrains/plugin/v/22967.svg)](https://plugins.jetbrains.com/plugin/22967)
+[![Downloads](https://img.shields.io/jetbrains/plugin/d/22967.svg)](https://plugins.jetbrains.com/plugin/22967)
+
+
+
+
+
+
+
+
+# LiveLatex JetBrains Plugin
+
+<!-- Plugin description -->
+
+LiveLatex: An Intellij IDEA plugin.
+
+**Work in progress**: 😎
+A LaTeX editor with live preview and syntax highlighting.
+<h1 align="center">
+    <a href="">
+      <img src="https://raw.githubusercontent.com/bg-omar/LiveLatex/refs/heads/master/src/main/resources/META-INF/pluginIcon.svg?sanitize=true" width="84" height="84" alt="logo"/>
+    </a><br/>
+    LiveLatex
+</h1>
+<!-- Plugin description end -->
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,26 @@
+import java.io.File
+import kotlin.text.substringAfter
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.markdownToHTML
+
+interface Injected {
+    @get:Inject val fs: FileSystemOperations
+}
+val injected = project.objects.newInstance<Injected>()
+fun properties(key: String) = providers.gradleProperty(key)
+
+val pluginVersion: String by project
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
     id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.changelog") version "2.2.0"
 }
 
 group = "com.omariskandarani"
-version = "1.0-SNAPSHOT"
+version = properties("pluginVersion").orNull ?: project.version.toString()
+
 
 repositories {
     mavenCentral()
@@ -14,8 +29,6 @@ repositories {
     }
 }
 
-// Configure IntelliJ Platform Gradle Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
         create("IC", "2025.1")
@@ -39,6 +52,37 @@ intellijPlatform {
 }
 
 tasks {
+    patchPluginXml {
+        version = properties("pluginVersion").orNull ?: project.version.toString()
+        sinceBuild = properties("pluginSinceBuild").orNull
+        untilBuild = properties("pluginUntilBuild").orNull
+        updatePluginXml()
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            file("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").let { markdownToHTML(it) }
+        )
+
+    }
+    signPlugin {
+        certificateChainFile.set(file("./secrets/chain.crt"))
+        privateKeyFile.set(file("./secrets/private_encrypted.pem"))
+        password = System.getenv("PRIVATE_KEY_PASSWORD")
+    }
+    publishPlugin {
+        dependsOn("patchChangelog")
+        // dependsOn(generateUpdatePluginsXml)
+        token = System.getenv("PUBLISH_TOKEN")
+        channels = properties("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+
     // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "21"
@@ -50,4 +94,36 @@ kotlin {
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
+}
+
+tasks.register<Jar>("debugJar") {
+    archiveClassifier.set("debug")
+    from(sourceSets.main.get().output)
+    manifest {
+        attributes["Implementation-Title"] = "LiveLatex Debug"
+        attributes["Implementation-Version"] = version
+    }
+}
+
+tasks.register<Jar>("releaseJar") {
+    archiveClassifier.set("release")
+    from(sourceSets.main.get().output)
+    manifest {
+        attributes["Implementation-Title"] = "LiveLatex Release"
+        attributes["Implementation-Version"] = version
+    }
+    // Example: Add obfuscation/minification here if needed
+}
+
+
+fun updatePluginXml() {
+    val pluginXmlFile = File("src/main/resources/META-INF/plugin.xml")
+
+    val pluginXmlContent = pluginXmlFile.readText()
+
+
+
+
+    pluginXmlFile.writeText(pluginXmlContent)
+
 }
